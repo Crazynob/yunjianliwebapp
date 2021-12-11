11405844d4f407813fad799714e15629f0cfa492
Crazynob <2909534724@qq.com>
2021-12-11 12:35:42 +0800

    再次初始化项目

--- src/main/java/red/fuyun/yunjianli/YunjianliApplication.java
+++ src/main/java/red/fuyun/yunjianli/YunjianliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YunjianliApplication {
-
+//首次初始化
     public static void main(String[] args) {
         SpringApplication.run(YunjianliApplication.class, args);
     }
