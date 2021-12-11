9d3b529b6375b392bb53277c036f5e0f23554bec
Crazynob <2909534724@qq.com>
2021-12-11 12:34:01 +0800

    初始化项目

---
+++ src/main/java/red/fuyun/yunjianli/YunjianliApplication.java
@@ -0,0 +1,13 @@
+package red.fuyun.yunjianli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunjianliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunjianliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/red/fuyun/yunjianli/YunjianliApplicationTests.java
@@ -0,0 +1,13 @@
+package red.fuyun.yunjianli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunjianliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
